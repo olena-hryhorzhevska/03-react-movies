9539c0f12c4c8b6871e3a3842264c9f3b45dc076
Olena Hryhorzhevska <elenahry8@gmail.com>
2025-08-04 09:46:21 -0400

    moviegrid and moviemodal fixed

--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -18,7 +18,7 @@ export default function MovieGrid({ onSelect, movies }: MovieGridProps) {
               className={css.image}
               loading="lazy"
             />
-            <h2 className={css.title}>Movie title</h2>
+            <h2 className={css.title}>{ movie.title}</h2>
           </div>
         </li>
       ))}
--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -32,32 +32,37 @@ export default function MovieModal({ movie, onClose }: MovieModalProps) {
 
 
   return createPortal(
-      <div className={css.backdrop} role="dialog" aria-modal="true" onClick={handleBackdropClick}>
-        <div className={css.modal}>
-          <button
-            className={css.closeButton}
-            aria-label="Close modal"
-            onClick={onClose}
-          >
-            &times;
-          </button>
-          <img
-            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-            alt="movie_title"
-            className={css.image}
-          />
-          <div className={css.content}>
-            <h2>{movie.title}</h2>
-            <p>{movie.overview}</p>
-            <p>
-              <strong>Release Date:</strong> {movie.release_date}
-            </p>
-            <p>
-              <strong>Rating:</strong> {movie.vote_average} / 10
-            </p>
-          </div>
+    <div
+      className={css.backdrop}
+      role="dialog"
+      aria-modal="true"
+      onClick={handleBackdropClick}
+    >
+      <div className={css.modal}>
+        <button
+          className={css.closeButton}
+          aria-label="Close modal"
+          onClick={onClose}
+        >
+          &times;
+        </button>
+        <img
+          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+          alt={movie.title}
+          className={css.image}
+        />
+        <div className={css.content}>
+          <h2>{movie.title}</h2>
+          <p>{movie.overview}</p>
+          <p>
+            <strong>Release Date:</strong> {movie.release_date}
+          </p>
+          <p>
+            <strong>Rating:</strong> {movie.vote_average} / 10
+          </p>
         </div>
-      </div>,
+      </div>
+    </div>,
     document.body
   );
 }
